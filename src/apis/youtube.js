8ada308644af94667a97d28f237559e25031042b
damadorcegedim <daniel.amadorvillegas@cegedim.com>
2019-03-05 13:52:33 +0100

    Initial version

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const KEY = 'AIzaSyCZuCpgThif0KJT8phhbBzq94Mj9_6PC-4';
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
